c0dcf2bfc4408f5fdd6a9c1c99397ee52ddebbc2
Will Studabaker <will.studabaker@gmail.com>
2021-02-05 16:13:57 -0500

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+let modal = document.getElementById("modal")
+modal.className = "hidden"
+document.addEventListener("DOMContentLoaded", () => {
+  let hearts = document.querySelectorAll("li.like > span")
+  for (const heart of hearts) {
+    heart.addEventListener('click', ()=>{
+        mimicServerCall().then(function(response){
+          if (heart.className == "like-glyph"){
+            heart.innerHTML = FULL_HEART
+            heart.className = "activated-heart"
+          } else {
+            heart.innerHTML = EMPTY_HEART
+            heart.className = "like-glyph"
+          }
+        })
+        .catch(function(error){
+          modal.hidden = false
+          setTimeout(function(){
+            modal.hidden = true
+          }, 5000)
+        })
+    })
+  }
+})
 
 
 
